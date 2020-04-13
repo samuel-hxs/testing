f1b05f6f816befc4afc97214aa1e2663e759855e
Hex3Dnone <samuel.traue@gmail.com>
2020-04-13 14:26:22 +0200

    Added missing '|| exit'

--- do.sh
+++ do.sh
@@ -7,7 +7,7 @@ mkdir -p bin/rel
 
 # Debug-<deb, Release->rel
 conf=deb
-g++ -c src/main.cc -Iinc/ -o obj/main.o -std=c++11
-g++ -c src/coin.cc -Iinc/ -o obj/coin.o -std=c++11
+g++ -c src/main.cc -Iinc/ -o obj/main.o -std=c++11 || exit
+g++ -c src/coin.cc -Iinc/ -o obj/coin.o -std=c++11 || exit
 
 g++ obj/main.o obj/coin.o -o bin/$conf/proc -lsfml-graphics -lsfml-window -lsfml-system -Llib/ -lBox2D -std=c++11
